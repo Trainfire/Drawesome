7c5c0a404051c15dd51182a45fbce421d407c564
Aaron <aarongarcha@gmail.com>
2016-02-02 00:12:44 +0000

    Small refactor.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,27 +1,49 @@
 using System;
+using System.Collections.Generic;
 using Fleck;
 
 namespace Server
 {
     class Program
     {
+        static List<WebSocketBehaviour> Handlers { get; set; }
+
         static void Main(string[] args)
         {
             var server = new WebSocketServer("ws://0.0.0.0:8181");
-            var room = new PlayerManager();
+
+            AddWebSocketBehaviourHandler(new PlayerManager());
+
             server.Start(socket =>
             {
-                socket.OnOpen += () => room.OnOpen(socket);
-                socket.OnClose += () => room.OnClose(socket);
-                socket.OnError += (ex) => room.OnError(ex);
-                socket.OnMessage += (str) => room.OnMessage(str);
-                socket.OnBinary += (bytes) => room.OnBinary(bytes);
+                socket.OnOpen += () => Handlers.ForEach(x => x.OnOpen(socket));
+                socket.OnClose += () => Handlers.ForEach(x => x.OnClose(socket));
+                socket.OnError += (ex) => Handlers.ForEach(x => x.OnError(ex));
+                socket.OnMessage += (str) => Handlers.ForEach(x => x.OnMessage(str));
+                socket.OnBinary += (bytes) => Handlers.ForEach(x => x.OnBinary(bytes));
             });
 
             Console.WriteLine("Press Enter to stop the server...");
             Console.ReadLine();
 
             server.Dispose();
+        }
+
+        static T AddWebSocketBehaviourHandler<T>(T instance) where T : WebSocketBehaviour
+        {
+            if (Handlers == null)
+                Handlers = new List<WebSocketBehaviour>();
+
+            if (Handlers.Contains(instance))
+            {
+                Console.WriteLine("Behaviours already contains {0}", instance.ToString());
+            }
+            else
+            {
+                Handlers.Add(instance);
+            }
+
+            return instance;
         }
     }
 }
