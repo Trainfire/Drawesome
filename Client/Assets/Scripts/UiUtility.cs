1a1c9ba970a1338d0a119f768e3a266dcae2a850
Aaron <aarongarcha@gmail.com>
2016-02-07 21:59:05 +0000

    Update UI utility

--- Client/Assets/Scripts/UiUtility.cs
+++ Client/Assets/Scripts/UiUtility.cs
@@ -2,11 +2,23 @@ using UnityEngine;
 
 public static class UiUtility
 {
-    public static T AddChild<T>(RectTransform parent, T prototype) where T : MonoBehaviour
+    /// <summary>
+    /// Adds a gameobject to a UI with the correct scale values.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="parent"></param>
+    /// <param name="prototype"></param>
+    /// <param name="enable">Specify if you want the child to be set active upon creation.</param>
+    /// <returns></returns>
+    public static T AddChild<T>(RectTransform parent, T prototype, bool enable = false) where T : MonoBehaviour
     {
         var instance = GameObject.Instantiate<T>(prototype);
-        instance.transform.localScale = Vector2.one;
         instance.transform.SetParent(parent.transform);
+        instance.transform.localScale = Vector2.one;
+
+        if (enable)
+            instance.gameObject.SetActive(true);
+
         return instance;
     }
 }
