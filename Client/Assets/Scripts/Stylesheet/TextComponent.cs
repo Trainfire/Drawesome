7e03c92823ddf77903aa8b69ae2d45860c310a3d
Aaron <aarongarcha@gmail.com>
2016-02-11 16:34:24 +0000

    More UI work

--- Client/Assets/Scripts/Stylesheet/TextComponent.cs
+++ Client/Assets/Scripts/Stylesheet/TextComponent.cs
@@ -17,6 +17,9 @@ namespace Stylesheet
             {
                 text.fontSize = TextData.TextSize;
                 text.lineSpacing = TextData.LineSpacing;
+
+                if (TextData.Color != null)
+                    text.color = TextData.Color.Color;
             }
         }
     }
--- Client/Assets/Scripts/Stylesheet/TextData.cs
+++ Client/Assets/Scripts/Stylesheet/TextData.cs
@@ -6,10 +6,13 @@ namespace Stylesheet
     {
         public int TextSize { get { return textSize; } }
         public float LineSpacing { get { return lineSpacing; } }
+        public ColorData Color { get { return colorData; } }
 
         [SerializeField]
         private int textSize;
         [SerializeField]
         private float lineSpacing = 1f;
+        [SerializeField]
+        private ColorData colorData;
     }
 }
