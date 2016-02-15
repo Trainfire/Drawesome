b35bbb568cb96f098c56469597a76dbd6b57078c
Aaron <aarongarcha@gmail.com>
2016-02-15 18:33:37 +0000

    [Client] Validate name input

--- Client/Assets/Scripts/Settings.cs
+++ Client/Assets/Scripts/Settings.cs
@@ -1,4 +1,6 @@
 public static class Settings
 {
     public const string HostUrl = "ws://127.0.0.1:8181/room";
+    public const int CharacterLimitMin = 3;
+    public const int CharacterLimitMax = 32;
 }
--- Client/Assets/Scripts/UI/UiLogin.cs
+++ Client/Assets/Scripts/UI/UiLogin.cs
@@ -4,12 +4,49 @@ using UnityEngine.UI;
 public class UiLogin : UiBase
 {
     public InputField InputName;
+    public UiInfoBox InfoBox;
     public Button Login;
+
+    enum ValidationResult
+    {
+        Success,
+        InvalidCharacterLength,
+    }
 
     public override void Initialise(Client client)
     {
         base.Initialise(client);
 
-        Login.onClick.AddListener(() => Client.Connection.Connect(InputName.text));
+        Login.onClick.AddListener(() =>
+        {
+            switch (IsInputValid(InputName.text))
+            {
+                case ValidationResult.Success:
+                    InfoBox.Hide();
+                    Client.Connection.Connect(InputName.text);
+                    break;
+                case ValidationResult.InvalidCharacterLength:
+                    InfoBox.Show(string.Format(Strings.CharacterLimit, Settings.CharacterLimitMin, Settings.CharacterLimitMax));
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        InfoBox.Hide();
+    }
+
+    ValidationResult IsInputValid(string input)
+    {
+        input = input.Trim();
+        input = input.Replace("\t", "");
+
+        if (input.Length > Settings.CharacterLimitMax)
+            return ValidationResult.InvalidCharacterLength;
+
+        if (input.Length < Settings.CharacterLimitMin)
+            return ValidationResult.InvalidCharacterLength;
+
+        return ValidationResult.Success;
     }
 }
