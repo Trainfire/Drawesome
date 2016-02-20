cb4ba850e9620d0ed5b8c938d5c338b1d28f93ea
Aaron <aarongarcha@gmail.com>
2016-02-20 23:19:00 +0000

    [Client] Add missing file

---
+++ Client/Assets/Scripts/SettingsLoader.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using System;
+using System.IO;
+using Newtonsoft.Json;
+
+public static class SettingsLoader
+{
+    const string SettingsFileName = "settings";
+
+    const string Prefix = "";
+    static string RootDirectory
+    {
+        get
+        {
+            return Application.dataPath;
+        }
+    }
+
+    static Settings settings;
+    public static Settings Settings
+    {
+        get
+        {
+            if (settings == null)
+                settings = Load<Settings>(SettingsFileName);
+            return settings;
+        }
+        set
+        {
+            Settings = settings;
+        }
+    }
+
+    static Settings Load()
+    {
+        return Load<Settings>(SettingsFileName);
+    }
+
+    static T Load<T>(string fileName)
+    {
+        var path = Path.Combine(RootDirectory, GetFileName(fileName));
+
+        if (File.Exists(path))
+        {
+            var file = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<T>(file);
+        }
+        else
+        {
+            Console.WriteLine("Failed to find '{0}'. Making default...", fileName);
+            return MakeDefault<T>(fileName);
+        }
+    }
+
+    static void Save<T>(T data, string fileName)
+    {
+        var json = JsonConvert.SerializeObject(data, Formatting.Indented);
+        File.WriteAllText(RootDirectory + GetFileName(fileName), json);
+    }
+
+    static T MakeDefault<T>(string fileName)
+    {
+        T data = Activator.CreateInstance<T>();
+        var json = JsonConvert.SerializeObject(data, Formatting.Indented);
+        File.WriteAllText(GetFileName(fileName), json);
+        return data;
+    }
+
+    static string GetFileName(string fileName)
+    {
+        return Prefix + "." + fileName;
+    }
+}
