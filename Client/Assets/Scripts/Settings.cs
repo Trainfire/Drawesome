bc6f0fa6497427eff7ee9e2ca0bbc418946148aa
Aaron <aarongarcha@gmail.com>
2016-02-29 14:54:03 +0000

    [Client] Update host

--- Client/Assets/Scripts/Settings.cs
+++ Client/Assets/Scripts/Settings.cs
@@ -1,6 +1,6 @@
 public class Settings
 {
-    public string HostUrl = "ws://188.166.145.195:8181";
+    public string HostUrl = "ws://drawesome.trainfire.net:8181";
     public int NameMinChars = 3;
     public int NameMaxChars = 32;
 
