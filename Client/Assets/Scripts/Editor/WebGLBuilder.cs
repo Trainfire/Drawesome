0c4f4203c2c1dc7f5b3ed663b8671807f1188230
Aaron <aarongarcha@gmail.com>
2016-03-01 16:32:31 +0000

    [Client] Update builder

--- Client/Assets/Scripts/Editor/WebGLBuilder.cs
+++ Client/Assets/Scripts/Editor/WebGLBuilder.cs
@@ -6,6 +6,6 @@ class WebGLBuilder
     static void build()
     {
         var scenes = EditorBuildSettings.scenes.Select(x => x.path).ToArray();
-        BuildPipeline.BuildPlayer(scenes, "WebGL-Dist", BuildTarget.WebGL, BuildOptions.None);
+        BuildPipeline.BuildPlayer(scenes, "WebGL", BuildTarget.WebGL, BuildOptions.None);
     }
 }
